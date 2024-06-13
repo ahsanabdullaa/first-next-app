c84f998973b6a2fe2f3e500367b6124c75bde2c1
ahsanabdullaa <ahsanidrees10@gmail.com>
2024-06-14 03:41:15 +0500

    ..

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AdToCart from './AdToCart'
+export default function ProductCard() {
+  return (
+    <div>
+        <AdToCart />
+    </div>
+  )
+}
---
+++ app/users/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import React from 'react'
+
+export default function UsersPage() {
+  return (
+    <div>UsersPage</div>
+
+  )
+}
